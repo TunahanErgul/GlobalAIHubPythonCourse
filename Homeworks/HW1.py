25c2d54ae2cf6b6a4745b7fc2a49d38b8d1c6f47
TunahanErgul <tunahanergul@gmail.com>
2021-04-07 19:53:00 +0300

    Update HW1.py
    Answer of first question has been added

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,23 @@
-#Explain your work
+#Swapping second half the list with first half of the list and printing swapped version to screen
 
 #Question 1
-for x in range(a):
-	print(a)
+
+#swap second half with second half 
+#list can be changed by user to test different cases
+
+mylist = [7,2,5,0,0,9,8]
+x = int(len(mylist)//2)
+j=x
+
+if int(len(mylist))%2 == 0:
+    while j != int(len(mylist)):
+        for i in range(x):
+            temp=mylist[i]
+            mylist[i]=mylist[j]
+            mylist[j]=temp
+            j=j+1
+else:
+    print("length of list must be even")
+   
+if int(len(mylist))%2 == 0:  
+    print(mylist)

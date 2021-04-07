ef882194d70084bc40c814c41b4586429452714e
TunahanErgul <tunahanergul@gmail.com>
2021-04-07 20:14:59 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,6 +1,7 @@
-#Swapping second half the list with first half of the list and printing swapped version to screen
 
 #Question 1
+#Swapping second half the list with first half of the list and printing swapped version to screen
+
 
 #swap second half with second half 
 #list can be changed by user to test different cases
@@ -21,3 +22,26 @@ else:
    
 if int(len(mylist))%2 == 0:  
     print(mylist)
+
+    
+    
+#Question 2
+#Finding even numbers according to user's input
+
+#Ask the user to input a single digit integer
+#to a variable "n"
+#then, print out all of the even numbers from 0 to n
+#including n
+
+n = int(input("Please enter a single digit integer \n"))
+
+if n < 10 and n > 0:
+    print("Even Numbers from 0 to {} are listed at below \n ".format(n))
+    evennumbers = []
+    for i in range(0,n+1):
+        if i % 2 == 0:
+            evennumbers.append(i)
+    print(evennumbers)
+
+else:
+    print("You have not entered a single digit integer ")

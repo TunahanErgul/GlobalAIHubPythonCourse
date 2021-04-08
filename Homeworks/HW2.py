a2c04c8bdac4e6e0bad20f28f0b0eb82d1226ee7
TunahanErgul <tunahanergul@gmail.com>
2021-04-08 20:07:58 +0300

    Update HW2.py
    Login attempts with dictionary usage

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,18 @@
-#Explain your work
+#login system with dictionary usage
 
-#Question 1
-for x in range(a):
-	print(a)
+accounts = {"admin":"administrator","ömer":"1234","tunahan":"12345"}
+
+print("Welcome to Internet Banking \n")
+print("To proceed, please enter your username and password correctly")
+
+username = input("Enter your username: \n")
+password = input("Enter your password: \n")
+
+
+if username in  accounts and accounts[username]==password:
+    print("You have succesfully entered to your account") 
+else:
+    print("You have  entered wrong username and password. \n ")
+    print("Please try again later")
+        
+        
